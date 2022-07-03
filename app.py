ec3cc21c5fc2af3422adcc094cf888f108e2044c
Emmanuel Osuolale <dmarinere@gmail.com>
2022-07-03 03:18:07 +0100

    successfully added time

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
 from flask import Flask, render_template
+from datetime import datetime 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     date = str(datetime.now()).split()[0]
+    print(date)
     return render_template('index.html', date=date)
+                            
 
 
 if __name__ == "__main__":
