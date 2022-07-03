e48ee2cc9298aaedacbc2053637290ba58a85ada
Osuolale Emmanuel <59930570+dmarinere@users.noreply.github.com>
2022-07-03 02:35:29 +0100

    added template to app py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     date = str(datetime.now()).split()[0]
-    return ""
+    return render_template('index.html', date=date)
 
 
 if __name__ == "__main__":
